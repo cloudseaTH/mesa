14fd68d957db84ba90e491a7d3c12abe97d07c32
Eduardo Lima Mitev <elima@igalia.com>
2017-11-14 18:03:25 +0100

    spirv: Accept SpvCapabilityStorageImageMultisample capability
    This seems to be a valid capability in SPIR-V shaders obtained from
    GLSL, that have a multisample image uniform.

--- src/compiler/spirv/spirv_to_nir.c
+++ src/compiler/spirv/spirv_to_nir.c
@@ -3433,6 +3433,7 @@ vtn_handle_preamble_instruction(struct vtn_builder *b, SpvOp opcode,
       case SpvCapabilityInputAttachment:
       case SpvCapabilityImageGatherExtended:
       case SpvCapabilityStorageImageExtendedFormats:
+      case SpvCapabilityStorageImageMultisample:
          break;
 
       case SpvCapabilityLinkage:
@@ -3440,7 +3441,6 @@ vtn_handle_preamble_instruction(struct vtn_builder *b, SpvOp opcode,
       case SpvCapabilityFloat16Buffer:
       case SpvCapabilityFloat16:
       case SpvCapabilityInt64Atomics:
-      case SpvCapabilityStorageImageMultisample:
       case SpvCapabilityInt8:
       case SpvCapabilitySparseResidency:
       case SpvCapabilityMinLod:
